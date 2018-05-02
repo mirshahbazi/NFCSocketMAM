da64e3d65a4a2ed81923c59dc1f4faeffb660393
mirshahbazi <mr.mirshahbazi@gmail.com>
2018-05-02 22:44:35 +0430

    fix some bugs

--- nfcsocket/src/androidTest/java/com/mam/nfcsocket/ApplicationTest.java
+++ nfcsocket/src/androidTest/java/com/mam/nfcsocket/ApplicationTest.java
@@ -3,9 +3,7 @@ package com.mam.nfcsocket;
 import android.app.Application;
 import android.test.ApplicationTestCase;
 
-/**
- * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
- */
+
 public class ApplicationTest extends ApplicationTestCase<Application> {
 
     public ApplicationTest() {
